a9208ecaee6fad57017c0f4bc403e6ba801bcdcd
Abbas firdous <abbasfathima40@gmail.com>
2026-03-10 05:26:39 +0530

    chore: building id updates

--- src/components/Map/BuildingLayer.tsx
+++ src/components/Map/BuildingLayer.tsx
@@ -18,7 +18,7 @@ export function BuildingLayer({
   const router = useNavigate();
 
   // fetch available building from db
-  const availableBlocksIds = ["69af585dd5577184a5f30ec1"];
+  const availableBlocksIds = ["69af5723e0e22516b8d96988"];
 
   const handleBuildingClick = (building: Building) => {
     console.log(building);
